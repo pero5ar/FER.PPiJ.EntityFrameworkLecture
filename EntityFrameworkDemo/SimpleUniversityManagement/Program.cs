9e91ddbe80dc0951b15ded81bed41051742494c7
pero5ar <pero.5ar@gmail.com>
2017-03-17 05:33:32 +0100

    remove unused directives

--- EntityFrameworkDemo/SimpleUniversityManagement/Program.cs
+++ EntityFrameworkDemo/SimpleUniversityManagement/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using SimpleUniversityManagement.Models;
 
--- EntityFrameworkDemo/SimpleUniversityManagement/UniversityDbContext.cs
+++ EntityFrameworkDemo/SimpleUniversityManagement/UniversityDbContext.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using SimpleUniversityManagement.Models;
 
 namespace SimpleUniversityManagement
